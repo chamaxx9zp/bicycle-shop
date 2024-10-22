ae7636df1f8fbbbc88411ba7ce1b34e707d20e98
chamaxx9zp <chamakapiyumal@gmail.com>
2024-10-22 20:48:56 +0530

    final comit

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['hebbkx1anhila5yf.public.blob.vercel-storage.com'], // Add the domain hosting your image
-      },
+    // output: "export",
 };
 
 export default nextConfig;
