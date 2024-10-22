60ceaf54971c9dfa636db2884466dd239a34fd12
chamaxx9zp <chamakapiyumal@gmail.com>
2024-10-22 22:44:41 +0530

    github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
+    assetPrefix: isProd ? '/bicycle-shop/' : '',
+    basePath: isProd ? '/bicycle-shop' : '',
+    trailingSlash: true, 
 };
 
 export default nextConfig;
